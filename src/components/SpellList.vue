dd7c221e646be1ebc727f4b074b96bcea4dff4ed
Krystian Nowakowski <krysti.8642@gmail.com>
2022-06-07 15:05:23 +0200

    added axios and temp Spellfilter

--- src/components/SpellList.vue
+++ src/components/SpellList.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { ref } from "vue";
 
 import Spell from "../interfaces/Spell";
 import SpellVue from "./Spell.vue";
 import SpellFormVue from "./SpellForm.vue";
+import SpellFilterVue from "./SpellFilter.vue";
 
 import { spells, classes, casting_times, schools } from "../main";
+
+const showAddForm = ref(false);
+const showFilterForm = ref(false);
 
 const deletespell = () => {
     spells.pop();
@@ -15,7 +19,14 @@ const deletespell = () => {
 <template>
     <div class="flex flex-col justify-items-center">
         <button @click="deletespell">deletespell</button>
-        <SpellFormVue />
+        <button @click="showAddForm = !showAddForm">
+            toggle add spell form
+        </button>
+        <button @click="showFilterForm = !showFilterForm">
+            toggle add spell filter
+        </button>
+        <SpellFilterVue v-if="showFilterForm" />
+        <SpellFormVue v-if="showAddForm" />
         <SpellVue v-for="spell in spells" :key="spell.spellId" :spell="spell" />
     </div>
 </template>
